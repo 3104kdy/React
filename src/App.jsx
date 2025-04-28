b63ecaaf55b356f1788691e4141852ddd3512335
3104kdy <3104kdy@naver.com>
2025-04-29 00:03:22 +0900

    route 세팅 test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
@@ -10,9 +11,13 @@ import New from './pages/New';
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+    </Routes>
+  );
 }
 
 export default App
+ 
