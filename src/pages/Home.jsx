05e2e286c0452823a87d09913685fcbe73558cfc
3104kdy <3104kdy@naver.com>
2025-05-01 19:27:59 +0900

    Query String 활용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
+// QueryStrings 사용 방법
+import { useSearchParams } from "react-router-dom";
+
+
 const Home = () => {
+    // 쿼리 스트링으로 전달된 파라미터를 가져올 수 있음
+    // const [params, setParams] = useSearchParams();
     return <div> Home</div>;
 }
 
