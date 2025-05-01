0a61cdb168639eeb2b122b2c7bbeb95e311416ac
3104kdy <3104kdy@naver.com>
2025-05-01 19:27:37 +0900

    url 파라미터 활용

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,6 +1,14 @@
+// url 파라미터를 가져오는 커스텀 훅
+import { useParams } from "react-router-dom";
+
+
 const Diary = () => {
+    const params = useParams();
+    console.log(params);
+
+
     return <div>
-        Diary
+        {params.id}번 일기
     </div>;
 }
 
