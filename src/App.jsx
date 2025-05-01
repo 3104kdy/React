2c30ee10c9d8c489ccc6fa7030e38fa205b7ef6e
3104kdy <3104kdy@naver.com>
2025-05-02 01:06:36 +0900

    state를 활용해서 일기 추가, 수정, 삭제 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useReducer } from 'react';
+import { useReducer, useRef } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
@@ -23,16 +23,87 @@ const mokData = [
   },
 ];
 
+// 매개변수로 action 객체를 전달
 function reducer(state, action) {
-  return state;
+  //state를 복사 (원본 데이터 복사 기능 ...)
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case 'DELETE':
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
 }
 
 function App() {
   // data라는 stat가 여러개의 일기를 보관하는 용도
   const [data, dispatch] = useReducer(reducer, mokData);
+  const idRef = useRef(3); //임시 데이터를 2번까지 만들어두었으니 3번 부터 생성
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    // action 객체임
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++, //신규가 생성될 때마다, 증가
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: 'UPDATE',
+      data: {
+        id: id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: 'DELETE',
+      id,
+    });
+  };
 
   return (
     <>
+      <button
+        onClick={() => {
+          onCreate(new Date().getTime(), 1, 'HI');
+        }}
+      >
+        일기 추가 테스트
+      </button>
+      <button
+        onClick={() => {
+          onUpdate(1, new Date().getTime(), 3, '!!!!!수정된 일기');
+        }}
+      >
+        일기 수정 테스트
+      </button>
+
+      <button
+        onClick={() => {
+          onDelete(1);
+        }}
+      >
+        일기 삭제 테스트
+      </button>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
