9ecda2d6ee665bc19d9a2d0270d181a81449c514
3104kdy <3104kdy@naver.com>
2025-05-01 18:35:04 +0900

    Link 활용

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Link, useNavigate} from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
+import NotFound from './pages/NotFound';
 
-// 1. "/" : 모든 일기를 조회하는 Home vpdlwl
+// Routes 컴포넌트 안에는 Route만 넣을 수 있음 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
+// 4, "/*" : 잘못된 경로에 대한 NotFound 페이지
 
 
 function App() {
+
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    // 새로운 페이지로 이동
+    nav("/new");
+  };
   return (
+    <>
+    <div>
+        <Link to="/">홈</Link>
+        <Link to="/new">새로운 일기</Link>
+        <Link to="/diary">일기 상세</Link>
+    </div>
+    <button onClick={onClickButton}>New 페이지로 이동</button>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
       <Route path="/diary" element={<Diary />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
+    </>
   );
 }
 
