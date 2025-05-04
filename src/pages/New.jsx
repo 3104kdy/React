94dcf513afa47c005afedf91ce016779935158aa
3104kdy <3104kdy@naver.com>
2025-05-04 15:54:35 +0900

    New 페이지 UI 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,7 +1,23 @@
+// Header 컴포넌트
+// Editor 컴포넌트
+// EmotionItem 컴포넌트 작성
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { useNavigate } from 'react-router-dom';
+
 const New = () => {
-    return <div>
-    New
-    </div>;
-}
+  const nav = useNavigate();
+
+  return (
+    <div>
+      <Header
+        title={'새 일기 쓰기'}
+        leftChild={<Button onClick={() => nav('/')} text={'< 뒤로가기'} />}
+      />
+      <Editor />
+    </div>
+  );
+};
 
 export default New;
