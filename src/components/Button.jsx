8225f400f35f7fe210371e14abf7b57ce2a47dec
3104kdy <3104kdy@naver.com>
2025-05-04 19:18:56 +0900

    일기 삭제하기 기능 구현

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 // props를 객체의 구조분해 할당을 이용해서 받아옴
 // text 와 type 으로 구분
 // 버튼이 실행되었을 떄 받아올 이벤트 함수 onClick
-const Button = ({ text, type, onClick }) => {
+const Button = ({ text, type, onClick = () => {} }) => {
   return (
     <button onClick={onClick} className={`Button Button_${type}`}>
       {text}
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,9 +1,47 @@
 import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
+
+// 절대경로로 import 시도
+import Button from '../components/Button';
+import Header from '@/components/Header';
+import Editor from '@/components/Editor';
+import { useNavigate } from 'react-router-dom';
 
 const Edit = () => {
+  const nav = useNavigate();
   const params = useParams();
-  console.log(params);
-  return <div>Edit</div>;
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    // window.confirm 에는 return 값이 boolean으로 존재함
+    // 확인을 누를 경우 = true 값을 반환
+    if (window.confirm('일기를 정말 삭제하시겠습니까?')) {
+      //일기 삭제로직 //App컴포넌트에 선언해둔 onDelete 함수 활용
+      onDelete(params.id);
+      nav('/', { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={'일기 수정하기'}
+        leftChild={
+          <Button
+            onClick={() => {
+              nav(-1);
+            }}
+            text={'< 뒤로가기'}
+          />
+        }
+        rightChild={
+          <Button onClick={onClickDelete} text={'삭제하기'} type={'NEGATIVE'} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
