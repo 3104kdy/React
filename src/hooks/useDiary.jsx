72b94775e869c3b107b2b01894588b8c8a38af27
3104kdy <3104kdy@naver.com>
2025-05-05 13:02:02 +0900

    Data가 전달되지 않는 에러 해결

--- src/hooks/useDiary.jsx
+++ src/hooks/useDiary.jsx
@@ -12,22 +12,25 @@ const useDiary = (id) => {
   const [curDiaryItem, setCurDiaryItem] = useState();
   const nav = useNavigate();
 
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(id)
-    );
+  useEffect(
+    () => {
+      const currentDiaryItem = data.find(
+        (item) => String(item.id) === String(id)
+      );
 
-    if (!currentDiaryItem) {
-      window.alert('존재하지 않는 일기');
-      // navigate 함수는
-      // 컴포넌트들이 다 마운트가 된 이후에 동작함
-      nav('/', { replace: true });
-    }
+      if (!currentDiaryItem) {
+        window.alert('존재하지 않는 일기');
+        // navigate 함수는
+        // 컴포넌트들이 다 마운트가 된 이후에 동작함
+        nav('/', { replace: true });
+      }
 
-    setCurDiaryItem(currentDiaryItem);
-    //onClickDelete 함수가 실행되고 나서도 useEffect가 진행됨
-  }, [id]); //useEffect 값이 비동기로 update되었기에, 의존성 배열에 data를 전달해주지 않는 방식으로 진행
-
+      setCurDiaryItem(currentDiaryItem);
+      //onClickDelete 함수가 실행되고 나서도 useEffect가 진행됨
+    },
+    [id],
+    data
+  );
   return curDiaryItem;
 };
 
